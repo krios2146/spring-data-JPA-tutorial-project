f9d54a3a75d132001a3681e98b8fd1e16c22d7d3
krios2146 <daun2146@yandex.ru>
2022-06-23 10:26:43 +0700

    Add StudentRepository, Add entry to DB in Application.java, Remove `id` from Student constructor

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(StudentRepository studentRepository) {
+        return args -> {
+            Student student = new Student("First Name", "Last Name", "Email", 30);
+            studentRepository.save(student);
+        };
     }
 
 }
